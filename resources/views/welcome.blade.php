87ee8868eef435846ba90cba53d0e98fcf1cadb6
Dimitrije Djurovic <dimitrije.djurovic@leaguengine.io>
2024-01-21 12:43:28 +0100

    Fix UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -61,7 +61,7 @@
                         <button type="button"
                                 @click="makeOrder"
                                 class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">
-                            <span v-if="!orderLoading">“Purchase”</span><i v-if="orderLoading"
+                            <span v-if="!orderLoading">Purchase</span><i v-if="orderLoading"
                                                                       class="fa fa-spinner"></i>
                         </button>
                         <button type="button"
@@ -96,7 +96,7 @@ https://cdn.jsdelivr.net/npm/axios@1.6.5/dist/axios.min.js
     createApp({
         setup() {
             const amount = ref(0)
-            const toCurrency = ref('EUR')
+            const toCurrency = ref('')
             const calculation = ref(null)
             const order = ref(null)
             const calculationLoading = ref(false)
